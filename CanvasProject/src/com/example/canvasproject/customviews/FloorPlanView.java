045ac7280e8028c15555432250a2efd693304e3b
Rahul Panuganti <rahulpanuganti91@gmail.com>
2015-11-19 15:28:09 +0530

    Completed zooming and panning.

--- CanvasProject/src/com/example/canvasproject/customviews/FloorPlanView.java
+++ CanvasProject/src/com/example/canvasproject/customviews/FloorPlanView.java
@@ -14,6 +14,7 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -23,6 +24,7 @@ import android.widget.ImageView;
 public class FloorPlanView extends ImageView{
 	
 	private double x,y;
+	private boolean init;
 	private Bitmap bitmap;
 	private Paint paint;
 	private ScaleGestureDetector detector;
@@ -36,7 +38,8 @@ public class FloorPlanView extends ImageView{
 	private int mode = 0;
 	private float startX = 0f,startY = 0f, translateX = 0f, translateY = 0f, 
 			displayWidth, displayHeight, lastGestureX, lastGestureY, 
-			zoomTranslationX = 0f, zoomTranslationY = 0f;
+			zoomTranslationX = 0f, zoomTranslationY = 0f, previousTranslateX = 0f, previousTranslateY = 0f,
+			dx, dy;
 	private Display display;
 	private Point dimensions;
 	
@@ -67,23 +70,24 @@ public class FloorPlanView extends ImageView{
 	
 	@Override
 	public void onDraw(Canvas canvas){
-		super.onDraw(canvas);
-		canvas.save();
-		if(translateX - zoomTranslationX > 0) {
-			translateX = zoomTranslationX;
+		Log.e("Scale2", translateX - previousTranslateX + "");
+		if(translateX + (getLimitLeft()) > 0) {
+			translateX = -getLimitLeft();
 		}
-		else if(((translateX - zoomTranslationX) * -1) > (scaleFactor - 1) * displayWidth) {
-			translateX = (1 - scaleFactor) * displayWidth +zoomTranslationX;
+		else if(translateX - getLimitRight() < 0) {
+			translateX = getLimitRight();
 			}
 		else {
 			
 		}
-		if(translateY - zoomTranslationY > 0) {
-			translateY = zoomTranslationY;
+		if(translateY  + getLimitTop()> 0) {
+			translateY = - getLimitTop();
 		}
-		else if(((translateY - zoomTranslationY) * -1) > (scaleFactor - 1) * displayWidth) {
-			translateY = (1 - scaleFactor) * displayWidth + zoomTranslationY;
+		else if(((translateY)) < getLimitBottom()) {
+			translateY = getLimitBottom();
 			}
+		super.onDraw(canvas);
+		canvas.save();
 		canvas.translate(translateX, translateY);
 		if(detector.isInProgress()) {
 			canvas.scale(scaleFactor, scaleFactor,detector.getFocusX(),detector.getFocusY());
@@ -94,15 +98,23 @@ public class FloorPlanView extends ImageView{
 		canvas.drawBitmap(bitmap, null, dst,null);
 		canvas.drawCircle((float) x, (float) y, 5/scaleFactor, paint);
 		canvas.restore();
+		if(init) {
+			displayWidth = getWidth();
+			dst = new Rect(0, 0, (int)displayWidth, (int)displayWidth);
+			init = false;
+			invalidate();
+		}
 	}
 	
 	public void init(Context context) {
+		init = true;
 		context.registerReceiver(pointReceiver, new IntentFilter("Coordinates"));
 		dimensions = new Point();
 		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
 		Display display = wm.getDefaultDisplay();
 		display.getSize(dimensions);
-		displayWidth = dimensions.x - 50;
+		//Log.e("Width", "" + getWidth());
+		displayWidth = dimensions.x;
 		dst = new Rect(0, 0, (int)displayWidth, (int)displayWidth);
 		paint = new Paint();
 		paint.setColor(Color.BLUE);
@@ -112,20 +124,28 @@ public class FloorPlanView extends ImageView{
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		detector.onTouchEvent(event);
 		switch(event.getAction() & MotionEvent.ACTION_MASK) {
 		case MotionEvent.ACTION_DOWN:
 			mode = DRAG;
-			startX = event.getX() - translateX;
-			startY = event.getY() - translateY;
+			startX = event.getX() - previousTranslateX;
+			startY = event.getY() - previousTranslateY;
 			break;
 		case MotionEvent.ACTION_MOVE:
 			if (detector.isInProgress()) {
 					lastGestureX = detector.getFocusX();
 					lastGestureY = detector.getFocusY();
 			}
+			dx = event.getX() - startX + previousTranslateX;
+			dy = event.getY() - startY + previousTranslateY;
+			
+			if(dx == 0 && dy == 0) {
+				mode = NONE;
+			}
 			if (mode == DRAG) {
-				translateX = event.getX() - startX;
-				translateY = event.getY() - startY;
+				Log.e("Scale", event.getX() - startX + "");
+				translateX = (event.getX() - startX);
+				translateY = (event.getY() - startY);
 			}
 			break;
 		case MotionEvent.ACTION_POINTER_DOWN:
@@ -133,16 +153,29 @@ public class FloorPlanView extends ImageView{
 			break;
 		case MotionEvent.ACTION_POINTER_UP:
 			mode = DRAG;
+			previousTranslateX = translateX;
+			previousTranslateY = translateY;
+			startX = event.getX() - previousTranslateX;
+			startY = event.getY() - previousTranslateY;
 			break;
 		case MotionEvent.ACTION_UP:
+			previousTranslateX = translateX;
+			previousTranslateY = translateY;
 			mode = NONE;
 			break;
 		}
-		boolean result = detector.onTouchEvent(event);
+		
+		
+		
+		if(scaleFactor == 1f){
+			translateX = 0f;
+			translateY = 0f;
+			invalidate();
+		}
 		if ((mode == DRAG && scaleFactor != 1f) || mode == ZOOM) {
 			invalidate();
 		}
-		return result;
+		return true;
 	}
 	
 	
@@ -153,11 +186,29 @@ public class FloorPlanView extends ImageView{
 			scaleFactor *= detector.getScaleFactor();
 			float scaleDifference = scaleFactor/oldScaleFactor;
 			scaleFactor = Math.max(MIN_ZOOM, Math.min(scaleFactor, MAX_ZOOM));
-			zoomTranslationX = ((1 - scaleDifference) * detector.getFocusX())/scaleFactor;
-			zoomTranslationY = ((1 - scaleDifference) * detector.getFocusY())/scaleFactor;
-			translateX -= zoomTranslationX;
-			translateY -= zoomTranslationY;
+			zoomTranslationX = ((1 - scaleDifference) * detector.getFocusX());
+			zoomTranslationY = ((1 - scaleDifference) * detector.getFocusY());
+			//translateX -= zoomTranslationX;
+			//translateY -= zoomTranslationY;
+			previousTranslateX = translateX;
+			previousTranslateY = translateY;
 			return true;
 		}
+	}
+	
+	public float getLimitLeft() {
+		return (1 - scaleFactor) * lastGestureX;
+	}
+	
+	public float getLimitRight() {
+		return (1 - scaleFactor) * (displayWidth - lastGestureX);
+	}
+	
+	public float getLimitTop() {
+		return (1 - scaleFactor) * lastGestureY;
+	}
+	
+	public float getLimitBottom() {
+		return (1 - scaleFactor) * (displayWidth - lastGestureY);
 	}
 }
